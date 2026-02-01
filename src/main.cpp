1444a5110f0be9b25d98cdc6baade03159e68946
Nischal07bot <singhnischal953@gmail.com>
2026-02-01 12:17:19 +0530

    implement type

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   //TODO: Uncomment the code below to pass the first stage
+  std::vector<std::string> builtin={"exit","echo","type"};
   while(true){
     start:std::cout << "$ ";
   
@@ -18,6 +19,25 @@ int main() {
       std::string echo_text;
       echo_text = command.substr(5);
       std::cout << echo_text << std::endl;
+      continue;
+    }
+    else if(command.substr(0, 5) == "type ")
+    {
+      std::string check=command.substr(5);
+      bool flag=false;
+      for(int i=0;i<builtin.size();i++)
+      {
+        if(check==builtin[i])
+        {
+          std::cout << check << " is a shell builtin" << std::endl;
+          flag=true;
+          break;
+        }
+      }
+      if(!flag)
+      {
+        std::cout<<check<<": not found"<<std::endl;
+      }
       continue;
     }
     std::cout << command << ": command not found" << std::endl;
